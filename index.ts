553fcfbe4d6c3f7dbfc9550860f9e3ebf22c7b4e
AdrianChojnacki <chojnackiadrian1@gmail.com>
2022-02-13 21:36:00 +0100

    1 file modified, 1 added

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 class Example {
-  constructor(public readonly id: number, private readonly name: string) {}
+  constructor(public id: number, private readonly name: string) {}
 }
 
 const example = new Example(55, 'Samurajła');

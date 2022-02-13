1383f0e424681fb698979a75a5569ca5b5533572
AdrianChojnacki <chojnackiadrian1@gmail.com>
2022-02-13 21:28:27 +0100

    1 file modified

--- index.ts
+++ index.ts
@@ -1,9 +1,15 @@
 class Example {
-  id: number;
-  name: string;
+  public readonly id: number;
+  private name: string;
+  protected costam: string;
 
-  constructor(id: string, name: string) {
+  constructor(id: number, name: string, costam: string) {
     this.id = id;
     this.name = name;
+    this.costam = costam;
+  }
+
+  changeId() {
+    this.id = 5;
   }
 }
